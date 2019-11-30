2f25f055a49ccbe65165926526521d3364e50618
Ali Reza <alirezayahya@gmail.com>
2019-11-30 02:49:14 +0100

    add MustGenerateFlags

--- cliutil.go
+++ cliutil.go
@@ -11,6 +11,14 @@ import (
 	"github.com/urfave/cli"
 )
 
+func MustGenerateFlags(config interface{}) []cli.Flag {
+	flags, err := GenerateFlags(config)
+	if err != nil {
+		panic(err)	
+	}
+	return flags
+}
+
 func GenerateFlags(config interface{}) ([]cli.Flag, error) {
 	t := reflect.TypeOf(config)
 	if t.Kind() != reflect.Ptr {
@@ -178,126 +186,3 @@ func toSnakeCase(str string) string {
 	snake = matchAllCap.ReplaceAllString(snake, "${1}-${2}")
 	return strings.ToLower(snake)
 }
-
-// // AppHelpTemplate is the text template for the Default help topic.
-// // cli.go uses text/template to render templates. You can
-// // render custom help text by setting this variable.
-// const AppHelpTemplate = `Usage: {{if .UsageText}}{{.UsageText}}{{else}}potato {{if .VisibleFlags}}[options]{{end}}{{if .ArgsUsage}}{{.ArgsUsage}}{{else}} <agent|server>{{end}}{{end}}
-// Options:
-//    {{range $index, $option := .VisibleFlags}}{{if $index}}
-//    {{end}}{{$option}}{{end}}
-// `
-
-// func NewApp(name string, config interface{}) {
-// 	var config struct {
-// 		NodeID           string
-// 		LogLevel         string
-// 		ListenAddress    string
-// 		AdvertiseAddress string
-// 		MigrationPath    string
-// 		DatabaseDSN      string
-// 		SkipMigration    bool
-// 	}
-// 	cli.AppHelpTemplate = AppHelpTemplate
-
-// 	log := log.New(os.Stdout, "", 0)
-
-// 	app := cli.NewApp()
-// 	app.Version = printVersion()
-// 	app.Flags = []cli.Flag{
-// 		cli.StringFlag{
-// 			Name:  "env.file, e",
-// 			Usage: "Environment variable file path",
-// 		},
-// 		cli.StringFlag{
-// 			Name:        "node-id",
-// 			Usage:       "Node identifier",
-// 			EnvVar:      "NODE_ID",
-// 			Destination: &config.NodeID,
-// 		},
-// 		cli.StringFlag{
-// 			Name:        "log-level",
-// 			Usage:       "Log level",
-// 			Value:       "info",
-// 			EnvVar:      "LOG_LEVEL",
-// 			Destination: &config.LogLevel,
-// 		},
-// 		cli.StringFlag{
-// 			Name:        "listen-address",
-// 			Usage:       "Port to wait incoming request from client",
-// 			Value:       "0.0.0.0:9000",
-// 			EnvVar:      "LISTEN_ADDRESS",
-// 			Destination: &config.ListenAddress,
-// 		},
-// 		cli.StringFlag{
-// 			Name:        "advertise-address",
-// 			Usage:       "Port to advertise metrics over http",
-// 			Value:       "0.0.0.0:9001",
-// 			EnvVar:      "ADVERTISE_ADDRESS",
-// 			Destination: &config.AdvertiseAddress,
-// 		},
-// 		cli.StringFlag{
-// 			Name:        "migration-path",
-// 			Usage:       "Database migration path for updating servers",
-// 			Value:       "./migrations",
-// 			EnvVar:      "MIGRATION_PATH",
-// 			Destination: &config.MigrationPath,
-// 		},
-// 		cli.StringFlag{
-// 			Name:        "database-dsn",
-// 			Usage:       "Database data source name",
-// 			Value:       "postgres://potato:potato@localhost:5432/potato?sslmode=disable",
-// 			EnvVar:      "DATABASE_DSN",
-// 			Destination: &config.DatabaseDSN,
-// 		},
-// 		cli.BoolFlag{
-// 			Name:        "skip-migration",
-// 			Usage:       "Skip database migration",
-// 			EnvVar:      "SKIP_MIGRATION",
-// 			Destination: &config.SkipMigration,
-// 		},
-// 	}
-
-// 	app.Before = func(ctx *cli.Context) error {
-// 		if envFile := ctx.String("env.file"); envFile != "" {
-// 			return godotenv.Load(envFile)
-// 		}
-
-// 		if config.NodeID == "" {
-// 			config.NodeID, _ = os.Hostname()
-// 		}
-
-// 		return nil
-// 	}
-
-// 	app.Action = func(ctx *cli.Context) (err error) {
-// 		l := logrus.New()
-// 		l.SetLevel(logrus.InfoLevel)
-
-// 		dockerClient, err := client.NewEnvClient()
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		ctxx := context.Background()
-// 		switch ctx.Args().First() {
-// 		case "server":
-// 			if !config.SkipMigration {
-// 				err = migrateUp(config.MigrationPath, config.DatabaseDSN)
-// 			}
-// 			if err == nil {
-// 				err = server.NewServer(l, config.ListenAddress, config.DatabaseDSN).Serve(ctxx)
-// 			}
-// 		case "agent":
-// 			err = agent.NewAgent(l, dockerClient, config.NodeID, config.ListenAddress, config.AdvertiseAddress).Start(ctxx)
-// 		default:
-// 			return errors.New("This command takes one argument: <agent|server>\nFor additional help try 'potato -help'")
-// 		}
-// 		return
-// 	}
-
-// 	if err := app.Run(os.Args); err != nil {
-// 		log.Printf("%v", colors.Bold(colors.Red)(err))
-// 		os.Exit(1)
-// 	}
-// }
